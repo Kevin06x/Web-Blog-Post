b0a884530f88e66c032fd339550ef3a411bab9b1
Kevin06x <kevinzhang06x@gmail.com>
2024-04-30 09:56:59 -0700

    Fix

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
-    posts.reverse()
+    posts = posts.reverse()
     return render(request, 'blog/post_list.html', {'posts': posts})
