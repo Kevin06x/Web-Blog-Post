4a0d8fc9e14e5f1a0ed7024a946c56b404e19ced
Kevin06x <kevinzhang06x@gmail.com>
2024-04-30 09:43:30 -0700

    Fix Post Order

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,5 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    posts.reverse()
     return render(request, 'blog/post_list.html', {'posts': posts})
--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nvxlx&f#z(lll@99ynr5*ezxq-i3_brbzcs5)9hj3$8jc(%rc9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
